33e0337ebf3437fe10ce663fc760c04dbf7cfc38
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-06-05 18:20:58 +0200

    Update s24.py

--- s24.py
+++ s24.py
@@ -4,6 +4,8 @@
 #         self.val = x
 #         self.next = None
 
+#iteration method
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         pre, cur = None, head
@@ -13,3 +15,16 @@ class Solution:
             pre = cur
             cur = temp
         return pre
+    
+#Rekursion
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        #rekursion
+        def reklist(pre, cur):
+            if not cur:
+                return pre               #终止条件
+            rev = reklist(cur, cur.next) #按顺序递归链表
+            cur.next = pre               #重新分配指针，准备开始逆序
+            return rev                   #逆序链表
+        return reklist(None, head)
+
