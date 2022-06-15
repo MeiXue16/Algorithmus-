82b57ff9544a7c320caf40b2c71871d2f9a6b6f7
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-06-16 00:08:38 +0200

    Create s32_1.py
    Drucken binärer Bäume von oben nach unten. BFS 从上到下打印二叉树

---
+++ s32_1.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        #DFS method
+        if not root: return []
+        res, queue = [], collections.deque() #双端队列
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return res
