bea54652b9ffb4ce8e7a33c773a1383ec158fcd5
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-06-16 23:23:23 +0200

    Update s32_3.py

--- s32_3.py
+++ s32_3.py
@@ -8,16 +8,16 @@
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root: return []
-        res, que =[], collections.deque([root])
+        res, que = [], collections.deque()
+        que.append(root)
         while que:
             temp =collections.deque()
             for i in range(len(que)):
-                node = que.popleft()
-                if len(res) % 2 : temp.appendleft(node.val)
+                #永远只能从左边出值，否则深度可能不对
+                node =que.popleft()
+                if len(res)%2 : temp.appendleft(node.val) 
                 else: temp.append(node.val)
-                if node.left:
-                    que.append(node.left)
-                if node.right:
-                    que.append(node.right)
-            res.append(list(temp))
+                if node.left: que.append(node.left)
+                if node.right: que.append(node.right)
+            res.append(list(temp)) #注意这里把双端队列temp变成list
         return res
