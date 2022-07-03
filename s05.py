738076b216eb9ca6fcb5fbdc1362231d75785884
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-07-03 13:25:28 +0200

    Update s05.py

--- s05.py
+++ s05.py
@@ -11,3 +11,12 @@ class Solution:
 class Solution:
     def replaceSpace(self, s: str) -> str:
         return s.replace(" ","%20")    #替换函数
+
+  #method 3
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+        res= []
+        for i in s:
+            if i== ' ': res.append("%20")
+            else: res.append(i)
+        return "".join(res)
