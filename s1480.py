da9b1347c5b73be7df55af0ca6545c1436cb0efd
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-07-10 21:04:59 +0200

    Create s1480.py
    Dynamische Summe von eindimensionalen Arrays 一维数组的动态和

---
+++ s1480.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        temp= 0
+        for i in range(len(nums)):
+            nums[i] += temp
+            temp = nums[i]
+        return nums
