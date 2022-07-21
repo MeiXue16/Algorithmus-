d150741a748fd73df1d0c480c1616a26361df0f3
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-07-22 00:25:12 +0200

    Update s13.py

--- s13.py
+++ s13.py
@@ -1,5 +1,23 @@
 class Solution:
     def movingCount(self, m: int, n: int, k: int) -> int:
+        def sum(x):
+            sumx =0
+            while x!= 0:
+                sumx += x%10
+                x = x//10
+            return sumx
+        def dfs(i, j, si, sj):
+            if i >= m or j >= n or si+sj >k or (i,j) in visited:
+                return 0
+            visited.add((i,j))
+            return 1 + dfs(i+1, j, sum(i+1), sj)+ dfs(i, j+1, si, sum(j+1))
+        visited =set()
+        return dfs(0,0,0,0)
+    
+
+#method 2: setze m,n in (0,100)
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
         def dfs(i, j, si, sj):
             if i >= m or j >= n or k < si + sj or (i, j) in visited: return 0
             visited.add((i,j))
