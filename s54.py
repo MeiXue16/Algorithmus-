332d02936ceb5d8762771ad51d08e096a7adb782
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2022-08-01 15:46:19 +0200

    Update s54.py

--- s54.py
+++ s54.py
@@ -1,3 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# methode 1
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        def dfs(root): #定义倒序的序列。中序遍历（左根右）结果为递增序列，因此倒着就是倒序的：右根左
+            if (not root) or self.k==0: return
+            dfs(root.right)
+            self.k -= 1
+            if self.k ==0:
+                self.res = root.val
+                return
+            dfs(root.left)
+        
+        self.k = k
+        dfs(root)
+        return self.res
+
+# methode 2
 class Solution:
     def kthLargest(self, root: TreeNode, k: int) -> int:
         def dfs(root):
